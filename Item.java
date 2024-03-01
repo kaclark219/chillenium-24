f0642ca188c1bb216d23ca34b9a9bea9b804dc63
kaclark219 <kaclark219@gmail.com>
2024-02-29 22:27:45 -0600

    Add/Remove from Inventory feature with Item class.

--- Item.java
+++ Item.java
@@ -2,11 +2,27 @@ public class Item {
     private String name;
     private int quantity;
 
-    public getName() {
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
         return name;
     }
 
-    public getQuantity() {
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public int getQuantity() {
         return quantity;
     }
+
+    public void increaseQuantity(Item item) {
+        item.quantity++;
+    }
+
+    public void decreaseQuantity(Item item) {
+        item.quantity--;
+    }
+
 }
--- PlayerInventory.java
+++ PlayerInventory.java
@@ -1,23 +1,35 @@
 import java.util.ArrayList;
-public class PlayerInventory {
+public class PlayerInventory extends Item {
+
     private ArrayList<Item> items;
 
-    public void addItem(Item i) {
+    public void addItem(String name) {
         for (int i = 0; i < items.size(); i++) {
-            if (items.get(i).getName().equals(i.getName())) {
-                items.get(i).increaseQuantity();
+            if (items.get(i).getName().equals(name)) {
+                increaseQuantity(items.get(i));
                 return;
             }
         }
-        items.add(i);
+        Item new_item = new Item();
+        new_item.setName(name);
+        new_item.setQuantity(1);
+        items.add(new_item);
+        System.out.println("New item added: " + name);
     }
 
-    public void removeItem(Item i) {
+    public void removeItem(String name) {
         for (int i = 0; i < items.size(); i++) {
-            if (items.get(i).getName().equals(i.getName())) {
-                items.get(i).decreaseQuantity();
+            if (items.get(i).getName().equals(name)) {
+                decreaseQuantity(items.get(i));
                 return;
             }
+        }
+        System.out.println("Item not found. ERROR.");
+    }
+
+    public void printInventory() {
+        for (int i = 0; i < items.size(); i++) {
+            System.out.println(items.get(i).getName() + " ... " + items.get(i).getQuantity());
         }
     }
 
