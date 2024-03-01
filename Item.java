38748df5d1f20770959364efc7fe66d8276f8b4c
kaclark219 <kaclark219@gmail.com>
2024-02-29 22:11:45 -0600

    File updates

---
+++ Item.java
@@ -0,0 +1,12 @@
+public class Item {
+    private String name;
+    private int quantity;
+
+    public getName() {
+        return name;
+    }
+
+    public getQuantity() {
+        return quantity;
+    }
+}
--- PlayerInventory.java
+++ PlayerInventory.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
 public class PlayerInventory {
+    private ArrayList<Item> items;
+
+    public void addItem(Item i) {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getName().equals(i.getName())) {
+                items.get(i).increaseQuantity();
+                return;
+            }
+        }
+        items.add(i);
+    }
+
+    public void removeItem(Item i) {
+        for (int i = 0; i < items.size(); i++) {
+            if (items.get(i).getName().equals(i.getName())) {
+                items.get(i).decreaseQuantity();
+                return;
+            }
+        }
+    }
+
 }
