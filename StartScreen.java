f6506ebe456422d2eda5a10c3ae30867129bf3cc
kaclark219 <kaclark219@gmail.com>
2024-03-02 03:38:14 -0600

    Story text & button panel added.

--- StartScreen.java
+++ StartScreen.java
@@ -22,10 +22,11 @@ public class StartScreen extends JFrame {
 
     JFrame window;
     Container con;
-    JPanel title_name_panel, button_panel, credits_panel;
+    JPanel title_name_panel, button_panel, credits_panel, story_panel;
     JLabel title_name_label;
     Font title_font = new Font("Times New Roman", Font.PLAIN, 36);
-    JTextArea credits_text;
+    JTextArea credits_text, story_text;
+    public JButton advance_story = new JButton("Continue");
 
 
 
@@ -115,13 +116,32 @@ public class StartScreen extends JFrame {
 
 
         PlayerInventory player_inventory = new PlayerInventory();
-        player_inventory.addItem("Broken ID");
         player_inventory.addItem("Dented Water Bottle");
         InventoryPanel inven_panel = new InventoryPanel(player_inventory);
         inven_panel.setBounds(900, 25, 200, 200);
 
+        story_text = new JTextArea();
+        story_text.setBounds(0, 0, 700, 300);
+        story_text.setBackground(Color.black);
+        story_text.setForeground(Color.white);
+        story_text.setLineWrap(true);
+        story_text.setText("Your back hurts. The hastily made campsite last night didn't do you any favors, & the chronic fatigue is already beginning to set in. You check your pockets, your water bottle is still sitting next to you but your last bit of beef jerky is gone .. it must have been taken by the rodents last night.");
+
+        story_text.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        story_panel = new JPanel();
+        story_panel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        story_panel.setBounds(100, 500, 900, 300);
+        story_panel.setBackground(Color.black);
+        advance_story.setBackground(Color.black);
+        advance_story.setForeground(Color.white);
+
+        story_panel.add(story_text);
+        story_panel.add(advance_story);
+
         this.add(progress_panel);
         this.add(inven_panel);
+        this.add(story_panel);
 
         revalidate();
         repaint();
